c170b6b752b6c8b646e458a1d4e94c177a72ef13
Nato Boram <nato.boram@gmail.com>
2018-09-23 19:41:39 -0400

    colour constants

--- consts.go
+++ consts.go
@@ -40,3 +40,16 @@ const (
 	permPrivateDirectory os.FileMode = 0700
 	permPrivateFile      os.FileMode = 0600
 )
+
+// Colors
+const (
+	colourAdministrator = 0xfee69e
+	colourModerator     = 0x96eff5
+	colourLight         = colourAdministrator
+	colourAbsynthe      = 0x7efb7b
+	colourObsidian      = 0xfbb6a8
+	colourShadow        = 0xf9b3fc
+	colourEel           = 0xbec9f8
+	colourNPC           = 0x9c9c9c
+	colourBot           = 0x7289da
+)
