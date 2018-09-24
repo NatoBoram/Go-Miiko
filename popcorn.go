c98dfd79a246dcd80df08a1e41d44c361fa4e0a7
Nato Boram <nato.boram@gmail.com>
2018-09-24 02:53:33 -0400

    better popcorn

--- popcorn.go
+++ popcorn.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math"
 	"math/rand"
 	"strings"
@@ -15,31 +14,39 @@ import (
 // Popcorn command
 func popcorn(s *discordgo.Session, c *discordgo.Channel, m *discordgo.Message) (done bool) {
 
-	// Check for "pop-corn", "popcorn", "maïs soufflé", "maïs éclaté", "pop corn"
-	if strings.Contains(strings.ToLower(m.Content), "popcorn") || strings.Contains(strings.ToLower(m.Content), "pop-corn") || strings.Contains(strings.ToLower(m.Content), "maïs soufflé") || strings.Contains(strings.ToLower(m.Content), "maïs éclaté") || strings.Contains(strings.ToLower(m.Content), "pop corn") || strings.Contains(strings.ToLower(m.Content), "🍿") {
-
-		// Seed
-		seed := time.Now().UnixNano()
-		source := rand.NewSource(seed)
-		rand := rand.New(source)
-
-		if rand.Float64() <= 1/math.Pow(wheel.Phi(), 1) {
-
-			// It's popcorn time!
-			s.ChannelTyping(m.ChannelID)
-			_, err := s.ChannelMessageSend(m.ChannelID, getPopcornMessage())
-			if err != nil {
-				fmt.Println("Couldn't tell everyone how much I love popcorn. Sad :(")
-				fmt.Println("Author : " + m.Author.Username)
-				fmt.Println("Message : " + m.Content)
-				fmt.Println(err.Error())
-				return false
-			}
-
-			return true
+	// Check for popcorn keywords
+	contains := false
+	for _, keyword := range popcornStrings {
+		if strings.Contains(strings.ToLower(m.Content), keyword) {
+			contains = true
+			break
 		}
 	}
 
+	// Return if there's no popcorn in there
+	if !contains {
+		return false
+	}
+
+	// Seed
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// Increase the power of Phi to reduce chances.
+	if random.Float64() <= 1/math.Pow(wheel.Phi(), 1) {
+
+		// It's popcorn time!
+		s.ChannelTyping(c.ID)
+		_, err := s.ChannelMessageSend(c.ID, getPopcornMessage())
+		if err != nil {
+			printDiscordError("Couldn't tell everyone how much I love popcorn. Sad :(", nil, c, m, nil, err)
+			return false
+		}
+
+		// Popcorn was successful!
+		return true
+	}
+
+	// Random chance decided it wasn't popcorn time.
 	return false
 }
 
@@ -80,10 +87,8 @@ func getPopcornMessage() string {
 	}
 
 	// Seed
-	seed := time.Now().UnixNano()
-	source := rand.NewSource(seed)
-	rand := rand.New(source)
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	// Return
-	return popcornList[rand.Intn(len(popcornList))]
+	return popcornList[random.Intn(len(popcornList))]
 }
