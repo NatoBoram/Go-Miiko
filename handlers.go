aa4de531ea64d2e0fd42cb33c9ca003835230223
Nato Boram <nato.boram@gmail.com>
2018-09-24 18:57:47 -0400

    enabled her love

--- handlers.go
+++ handlers.go
@@ -202,7 +202,7 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	// Love!
-	// done = love(s, guild, channel, m.Message)
+	done = love(s, guild, channel, m.Message)
 	if done {
 		return
 	}
