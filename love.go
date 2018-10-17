476005229805867d0038597c77b87a36206418ff
Nato Boram <nato.boram@gmail.com>
2018-10-17 09:58:38 -0400

    fix get crush when there's no nickname

--- love.go
+++ love.go
@@ -39,54 +39,62 @@ func love(s *discordgo.Session, g *discordgo.Guild, c *discordgo.Channel, m *dis
 
 func getLoveMessage(m *discordgo.Member) string {
 
+	// Check for a valid nickname
+	var name string
+	if m.Nick == "" {
+		name = m.User.Username
+	} else {
+		name = m.Nick
+	}
+
 	// Messages
 	loveList := [...]string{
 
 		// Greetings
-		"Coucou **" + m.Nick + "** :3",
-		"Coucou **" + m.Nick + "**! \\*-*",
-		"Salut les gens... Oh! **" + m.Nick + "**! :heart:",
-		"Bonjour... Oh! **" + m.Nick + "**! :heart:",
-		"Coucou tout le monde... Oh! **" + m.Nick + "**! :heart:",
+		"Coucou **" + name + "** :3",
+		"Coucou **" + name + "**! \\*-*",
+		"Salut les gens... Oh! **" + name + "**! :heart:",
+		"Bonjour... Oh! **" + name + "**! :heart:",
+		"Coucou tout le monde... Oh! **" + name + "**! :heart:",
 		"Coucou mon amour!",
 
 		// Orders
-		"Tiens-moi la main, **" + m.Nick + "**",
-		"**" + m.Nick + "**! Regarde-moiii \\*-*",
+		"Tiens-moi la main, **" + name + "**",
+		"**" + name + "**! Regarde-moiii \\*-*",
 		"Caresse-moi les oreilles, s'il te plait!",
 
 		// Questions
-		"**" + m.Nick + "**... Tu veux qu'on fasse quelque chose ensemble?",
-		"Oh, **" + m.Nick + "**, est-ce que je te manque?",
-		"Est-ce que tu penses à moi, **" + m.Nick + "**?",
-		"**" + m.Nick + "**, me demanderas-tu ma main un jour..?",
-		"**" + m.Nick + "**, j'ai fait du popcorn, tu veux en manger avec moi? :3",
-		"**" + m.Nick + "**! Je suis là! Je t'ai manqué, n'est-ce pas? :smile:",
-		"**" + m.Nick + "**! Es-tu content du matelas que j'ai fait mettre dans ta chambre? J'ai dormi dessus :blush:",
+		"**" + name + "**... Tu veux qu'on fasse quelque chose ensemble?",
+		"Oh, **" + name + "**, est-ce que je te manque?",
+		"Est-ce que tu penses à moi, **" + name + "**?",
+		"**" + name + "**, me demanderas-tu ma main un jour..?",
+		"**" + name + "**, j'ai fait du popcorn, tu veux en manger avec moi? :3",
+		"**" + name + "**! Je suis là! Je t'ai manqué, n'est-ce pas? :smile:",
+		"**" + name + "**! Es-tu content du matelas que j'ai fait mettre dans ta chambre? J'ai dormi dessus :blush:",
 
 		// Reactions
 		":heart:",
 		"\\*Frissonne*",
 		"\\*-*",
-		"**" + m.Nick + "**-senpai \\*-*",
+		"**" + name + "**-senpai \\*-*",
 
 		// Verbose
-		"J'ai trouvé un morceau de cristal pour toi, **" + m.Nick + "** :heart:",
+		"J'ai trouvé un morceau de cristal pour toi, **" + name + "** :heart:",
 		"Cette voix est une musique à mes oreilles",
-		"J'aimerais pouvoir passer plus de temps avec toi, **" + m.Nick + "**...",
-		"Je fais juste passer pour dire à **" + m.Nick + "** que je l'aime!",
+		"J'aimerais pouvoir passer plus de temps avec toi, **" + name + "**...",
+		"Je fais juste passer pour dire à **" + name + "** que je l'aime!",
 		"J'adore quand tu parles... :3",
 		"J'adore entendre mon amour parler \\*-*",
-		"Mais quelle est cette douce musique? ... Oh! C'est la voix de **" + m.Nick + "**!",
+		"Mais quelle est cette douce musique? ... Oh! C'est la voix de **" + name + "**!",
 
 		// Actions
-		"\\*Pense à **" + m.Nick + "***",
-		"\\*Regarde **" + m.Nick + "***",
-		"\\*Se languis de **" + m.Nick + "***",
+		"\\*Pense à **" + name + "***",
+		"\\*Regarde **" + name + "***",
+		"\\*Se languis de **" + name + "***",
 
 		// Also fits in Command
-		"**" + m.Nick + "**, je t'aime!",
-		"Aaah... **" + m.Nick + "**!",
+		"**" + name + "**, je t'aime!",
+		"Aaah... **" + name + "**!",
 	}
 
 	// Seed
@@ -116,44 +124,52 @@ func getLoverCmd(s *discordgo.Session, g *discordgo.Guild, c *discordgo.Channel,
 
 func getLoverMessage(m *discordgo.Member) string {
 
+	// Check for a valid nickname
+	var name string
+	if m.Nick == "" {
+		name = m.User.Username
+	} else {
+		name = m.Nick
+	}
+
 	// Messages
 	loveList := [...]string{
 
 		// Verbose
-		"Je crois... Je crois que j'aime **" + m.Nick + "**.",
-		"Je crois... Je crois que j'ai un faible pour **" + m.Nick + "**.",
-		"Disons que je chéris particulièrement **" + m.Nick + "**.",
-		"Si j'avais à marier quelqu'un... Ce serait **" + m.Nick + "**!",
-		"Peut-être... **" + m.Nick + "**?",
-		"Je planifie mon mariage avec **" + m.Nick + "**!",
-		"J'avoue avoir un faible pour **" + m.Nick + "**.",
-		"Lance, c'est du passé. **" + m.Nick + "**, c'est mon futur!",
-		"Je l'admets... je rêve de **" + m.Nick + "** la nuit...",
-		"J'avoue que... je rêve de **" + m.Nick + "** la nuit.",
-		"**" + m.Nick + "** est le beurre sur mon popcorn!",
-		"*Si seulement **" + m.Nick + "** m'aimait autant que je l'aime...*",
-		"Je n'avouerai jamais que j'aime **" + m.Nick + "**!",
-		"Non! Vous ne saurez jamais que j'aime **" + m.Nick + "**!",
+		"Je crois... Je crois que j'aime **" + name + "**.",
+		"Je crois... Je crois que j'ai un faible pour **" + name + "**.",
+		"Disons que je chéris particulièrement **" + name + "**.",
+		"Si j'avais à marier quelqu'un... Ce serait **" + name + "**!",
+		"Peut-être... **" + name + "**?",
+		"Je planifie mon mariage avec **" + name + "**!",
+		"J'avoue avoir un faible pour **" + name + "**.",
+		"Lance, c'est du passé. **" + name + "**, c'est mon futur!",
+		"Je l'admets... je rêve de **" + name + "** la nuit...",
+		"J'avoue que... je rêve de **" + name + "** la nuit.",
+		"**" + name + "** est le beurre sur mon popcorn!",
+		"*Si seulement **" + name + "** m'aimait autant que je l'aime...*",
+		"Je n'avouerai jamais que j'aime **" + name + "**!",
+		"Non! Vous ne saurez jamais que j'aime **" + name + "**!",
 
 		// Tsundere
-		"C'est pas comme si j'aimais **" + m.Nick + "** ou quoi que ce soit...",
-		"**" + m.Nick + "**, mais... Ne te fais pas de fausses idées!",
+		"C'est pas comme si j'aimais **" + name + "** ou quoi que ce soit...",
+		"**" + name + "**, mais... Ne te fais pas de fausses idées!",
 
 		// Exclamations
-		"**" + m.Nick + "**, évidemment!",
-		"**" + m.Nick + "**, sans aucun doute!",
-		"Que... Quoi? Ce... Je... **" + m.Nick + "**!",
-		"**" + m.Nick + "** d'amour :heart:",
-		"JE N'AVOUERAI JAMAIS! ... **" + m.Nick + "**.",
+		"**" + name + "**, évidemment!",
+		"**" + name + "**, sans aucun doute!",
+		"Que... Quoi? Ce... Je... **" + name + "**!",
+		"**" + name + "** d'amour :heart:",
+		"JE N'AVOUERAI JAMAIS! ... **" + name + "**.",
 
 		// Straight answers
-		"**" + m.Nick + "** est l'amour de ma vie.",
-		"À part le popcorn? **" + m.Nick + "**.",
-		"Je suis amoureuse de **" + m.Nick + "**.",
+		"**" + name + "** est l'amour de ma vie.",
+		"À part le popcorn? **" + name + "**.",
+		"Je suis amoureuse de **" + name + "**.",
 
 		// Also fits in Bot
-		"**" + m.Nick + "**, je t'aime!",
-		"Aaah... **" + m.Nick + "**!",
+		"**" + name + "**, je t'aime!",
+		"Aaah... **" + name + "**!",
 	}
 
 	// Seed
