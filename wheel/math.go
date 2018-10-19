db3552b10890647efe665647eee142c0eff52c82
Nato Boram <nato.boram@gmail.com>
2018-10-19 00:28:47 -0400

    ToFrenchDate

--- wheel/math.go
+++ wheel/math.go
@@ -3,6 +3,7 @@ package wheel
 import (
 	"math"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -50,4 +51,40 @@ func StringInSlice(str string, list []string) bool {
 // 100 = 0%
 func RandomOverPhiPower(power float64) bool {
 	return rand.New(rand.NewSource(time.Now().UnixNano())).Float64() <= 1/math.Pow(Phi(), power)
+}
+
+// ToFrenchDate takes a time and transforms it into a human-readable format.
+func ToFrenchDate(t time.Time) string {
+	year, month, day := t.Date()
+
+	// French months
+	var smonth string
+	switch month {
+	case time.January:
+		smonth = "Janvier"
+	case time.February:
+		smonth = "Février"
+	case time.March:
+		smonth = "Mars"
+	case time.April:
+		smonth = "Avril"
+	case time.May:
+		smonth = "Mai"
+	case time.June:
+		smonth = "Juin"
+	case time.July:
+		smonth = "Juillet"
+	case time.August:
+		smonth = "Août"
+	case time.September:
+		smonth = "Septembre"
+	case time.October:
+		smonth = "Octobre"
+	case time.November:
+		smonth = "Novembre"
+	case time.December:
+		smonth = "Décembre"
+	}
+
+	return strconv.Itoa(day) + " " + smonth + " " + strconv.Itoa(year)
 }
